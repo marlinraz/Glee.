ae991e4d93fc5c3c528f66d5d8bde090a4c76c80
marlinraz <razuvaeva.yana@mail.ru>
2022-03-03 10:24:39 +0700

    added page index

--- app/js/main.js
+++ app/js/main.js
@@ -6,4 +6,20 @@ $(function(){
         autoplay: true,
         autoplaySpeed: 2000
     });
+
+    /* var mixer = mixitup('.design__items');
+    var mixer = mixitup('.product__items'); */
+
+
+    var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+ 
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+ 
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 })
